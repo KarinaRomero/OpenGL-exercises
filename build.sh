d4fc20fa068d5021c335368d8d3d9f5ec02b68fc
fenix <betzy_skamm@hotmail.com>
2018-11-21 00:29:35 -0800

    added sfml andconfigured development environment

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#This is a bash file
+clang++ main.cpp -std=c++11 -F./frameworks -framework SFML -framework sfml-audio -framework sfml-graphics -framework sfml-network -framework sfml-system -framework sfml-window -o build/openglP -O0 -g
